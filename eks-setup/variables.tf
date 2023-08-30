8bd72b2fdd8f49037735e0c636212fa89a763ab9
tomershalev9 <tomershalev9@gmail.com>
2023-08-30 18:42:15 +0300

    added all terraform files

---
+++ eks-setup/variables.tf
@@ -0,0 +1,11 @@
+variable "cluster-name" {
+  description = "Enter eks cluster name - example like eks-demo, eks-dev etc"
+  type        = string
+  default     = "demo"
+}
+
+variable "create_resource" {
+  type        = bool
+  default     = true
+  description = "controls whether the resource should be created"
+}
